6067c1a6f5b2a53633e21a73918fb5ca9cdc8b7d
Prince Verma <princeverma.codes@gmail.com>
2026-02-15 18:21:44 +0530

    learned forLoop

---
+++ src/com/myPractice/areaOfRectangle.java
@@ -0,0 +1,11 @@
+package com.myPractice;
+
+public class areaOfRectangle {
+    public void aor(double length, double width) {
+        
+        double area =  width * length;
+        System.out.println("Area of Rectangle: " + area);
+       
+
+    }
+}
--- src/com/myPractice/theMain.java
+++ src/com/myPractice/theMain.java
@@ -4,8 +4,12 @@ public class theMain {
     public static void main(String[] args)  {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello Java");
-        songLoop song = new songLoop();
-        song.songL();
+        System.out.println("Enter length of rectangle: ");
+        double length = scan.nextDouble();
+        System.out.println("Enter width of rectangle: ");
+        double width = scan.nextDouble();
+        areaOfRectangle aor = new areaOfRectangle();
+        aor.aor(length, width);
         scan.close();
     }
 }
